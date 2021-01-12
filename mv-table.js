1e701f4e0fffed575da5a1d692c293cbca57184a
Tony Alejandro <tonysviews@gmail.com>
2021-01-12 18:12:47 +0800

    add selectOne property

--- mv-table.js
+++ mv-table.js
@@ -7,30 +7,46 @@ import "./cell_types/mv-text.js";
 import "./cell_types/mv-url.js";
 import "./cell_types/mv-image.js";
 
-const CELL_TYPES = props => {
+const CELL_TYPES = (props) => {
   const { row, column, datePattern } = props;
   const { name, target } = column;
   const value = row[name] || {};
   return {
     ARRAY: html`<mv-array .value="${value}"></mv-array>`,
-    DATE: html`<mv-date .value="${value}" .datePattern="${datePattern}"></mv-date>`,
+    DATE: html`<mv-date
+      .value="${value}"
+      .datePattern="${datePattern}"
+    ></mv-date>`,
     STRING: html`<mv-text .value="${value}"></mv-text>`,
     TEXT: html`<mv-text .value="${value}"></mv-text>`,
-    URL: html`<mv-url .href="${value.href}" .label="${value.label}" .target="${target}"></mv-url>`,
-    IMAGE: html`<mv-image .href="${value.href}" .alt="${value.alt}" .title="${value.title}" .content="${value.content}"></mv-image>`
+    URL: html`<mv-url
+      .href="${value.href}"
+      .label="${value.label}"
+      .target="${target}"
+    ></mv-url>`,
+    IMAGE: html`<mv-image
+      .href="${value.href}"
+      .alt="${value.alt}"
+      .title="${value.title}"
+      .content="${value.content}"
+    ></mv-image>`,
   };
 };
 
 const SELECT_ALL = { id: "all", value: "all" };
 
-const getCellComponent = props => {
-  const { column: { type } } = props;
+const getCellComponent = (props) => {
+  const {
+    column: { type },
+  } = props;
   return CELL_TYPES(props)[type] || CELL_TYPES(props)["TEXT"];
 };
 
 const getStyle = (columnsStyle, column, type) => {
-  if (columnsStyle.some(columnStyle => columnStyle.name === column)) {
-    return columnsStyle.find(columnStyle => columnStyle.name === column)[type]
+  if (columnsStyle.some((columnStyle) => columnStyle.name === column)) {
+    return columnsStyle.find((columnStyle) => columnStyle.name === column)[
+      type
+    ];
   }
   return "";
 };
@@ -41,16 +57,17 @@ export class MvTable extends LitElement {
       rows: { type: Array, attribute: false, reflect: true },
       columns: { type: Array, attribute: true },
       selectable: { type: Boolean, attribute: true },
+      selectOne: { type: Boolean, attribute: "select-one", reflect: true },
       "with-checkbox": { type: Boolean, attribute: true },
       "checkbox-column-label": { type: String, attribute: true },
       "action-column": { type: Object, attribute: true },
-      selectedRows: { type: Array, attribute: false },
+      selectedRows: { type: Array, attribute: false, reflect: true },
 
       //  valid theme values are: "light", "dark"
       //    default: "light"
       theme: { type: String, attribute: false },
       datePattern: { type: String, attribute: "date-pattern", reflect: true },
-      columnsStyle: { type: Array, attribute: false }
+      columnsStyle: { type: Array, attribute: false },
     };
   }
 
@@ -59,17 +76,23 @@ export class MvTable extends LitElement {
       :host {
         font-family: var(--font-family, Arial);
         font-size: var(--font-size-m, 10pt);
-        --table-header-font-family: var(--mv-table-header-font-family, var(--font-family, Arial));
+        --table-header-font-family: var(
+          --mv-table-header-font-family,
+          var(--font-family, Arial)
+        );
         --table-row-height: var(--mv-table-row-height, 66px);
         --table-row-cursor: var(--mv-table-row-cursor, default);
-        
-        --head-light-background: var(--mv-table-head-light-background, #F5F6FA);
+
+        --head-light-background: var(--mv-table-head-light-background, #f5f6fa);
         --body-light-background: var(--mv-table-body-light-background);
-        --hover-light-background: var(--mv-table-hover-light-background, #EDEDED);
-        
-        --head-dark-background: var(--mv-table-head-dark-background, #23404C);
-        --body-dark-background: var(--mv-table-body-dark-background, #373E48);
-        --hover-dark-background: var(--mv-table-hover-dark-background, #4E686D);
+        --hover-light-background: var(
+          --mv-table-hover-light-background,
+          #ededed
+        );
+
+        --head-dark-background: var(--mv-table-head-dark-background, #23404c);
+        --body-dark-background: var(--mv-table-body-dark-background, #373e48);
+        --hover-dark-background: var(--mv-table-hover-dark-background, #4e686d);
         --color: var(--mv-table-color);
       }
 
@@ -106,15 +129,20 @@ export class MvTable extends LitElement {
       }
 
       tbody tr {
-        border-bottom: 1px solid #E9E9E9;
+        border-bottom: 1px solid #e9e9e9;
         cursor: var(--table-row-cursor);
         background-color: var(--body-background);
       }
 
-      tbody tr:hover, tbody tr.selected {
+      tbody tr:hover,
+      tbody tr.selected {
         background-color: var(--hover-background);
       }
-      
+
+      tbody tr.selectable {
+        cursor: pointer;
+      }
+
       td {
         border-bottom: none;
         padding: 0 15px 0 15px;
@@ -140,27 +168,27 @@ export class MvTable extends LitElement {
       }
 
       .numeric {
-        text-align: right;        
+        text-align: right;
       }
 
       mv-checkbox {
         margin: auto;
       }
-      
+
       .light {
         --head-background: var(--head-light-background);
         --body-background: var(--body-light-background);
         --hover-background: var(--hover-light-background);
-        --color: #80828C;
+        --color: #80828c;
         --mv-checkbox-border-color: var(--color);
         --mv-table-url-color: var(--color);
       }
-      
+
       .dark {
         --head-background: var(--head-dark-background);
         --body-background: var(--body-dark-background);
         --hover-background: var(--hover-dark-background);
-        --color: #FFFFFF;
+        --color: #ffffff;
         --mv-checkbox-border-color: var(--color);
         --mv-table-url-color: var(--color);
       }
@@ -172,6 +200,7 @@ export class MvTable extends LitElement {
     this.columns = [];
     this.rows = [];
     this.selectable = false;
+    this.selectOne = false;
     this.selectedRows = [];
     this["with-checkbox"] = false;
     this["checkbox-column-label"] = "";
@@ -189,52 +218,71 @@ export class MvTable extends LitElement {
 
     return html`
       <div class="mv-table-container ${this.theme}">
-      <table>
-        <thead>
-          <tr>
-          ${withCheckbox
-            ? html`
-              <td @click="${this.handleCellClick()}">
-                <mv-checkbox
-                  .value="${SELECT_ALL}"
-                  .checked="${this.isAllSelected}"
-                  @click-checkbox="${this.handleClickCheckbox}"
-                  label="${this["checkbox-column-label"]}"
-                > </mv-checkbox>
-              </td>`
-            : html``}
-          ${this.columns.map(column => html`<td style="${getStyle(this.columnsStyle, column.name, "header")}">${column.title}</td>`)}
-          ${hasActionColumn
-            ? html`
-            <td class="action-header">
-              ${this["action-column"].label}
-            </td>`
-            : html``}
-          </tr>
-        </thead>
-        <tbody>
-          ${this.rows.map(row => {
-            const selected = this.selectedRows.includes(row);
-            const rowClass = `mv-table-row${selected ? " selected" : ""}`;
-            return html`
-                <tr
-                  @click="${this.handleRowClick(row)}"
-                  class="${rowClass}"
-                >
+        <table>
+          <thead>
+            <tr>
+              ${withCheckbox
+                ? html` <td @click="${this.handleCellClick()}">
+                    <mv-checkbox
+                      .value="${SELECT_ALL}"
+                      .checked="${this.isAllSelected}"
+                      @click-checkbox="${this.handleClickCheckbox}"
+                      label="${this["checkbox-column-label"]}"
+                    >
+                    </mv-checkbox>
+                  </td>`
+                : html``}
+              ${this.columns.map(
+                (column) =>
+                  html`<td
+                    style="${getStyle(
+                      this.columnsStyle,
+                      column.name,
+                      "header"
+                    )}"
+                  >
+                    ${column.title}
+                  </td>`
+              )}
+              ${hasActionColumn
+                ? html` <td class="action-header">
+                    ${this["action-column"].label}
+                  </td>`
+                : html``}
+            </tr>
+          </thead>
+          <tbody>
+            ${this.rows.map((row) => {
+              const selected = this.selectedRows.includes(row);
+              const rowClass = `mv-table-row${selected ? " selected" : ""}${
+                this.selectable || this.selectOne ? " selectable" : ""
+              }`;
+              return html`
+                <tr @click="${this.handleRowClick(row)}" class="${rowClass}">
                   ${withCheckbox
-                    ? html`
-                      <td>
+                    ? html` <td>
                         <mv-checkbox
                           .value="${row}"
                           .checked="${selected}"
                           @click-checkbox="${this.handleClickCheckbox}"
-                        > </mv-checkbox>
+                        >
+                        </mv-checkbox>
                       </td>`
                     : html``}
-                  ${this.columns.map(column => {
-                    const cellComponent = getCellComponent({ row, column, datePattern });
-                    return html`
-                    <td @click="${this.handleCellClick(row, column)}" style="${getStyle(this.columnsStyle, column.name, "cell")}">
+                  ${this.columns.map((column) => {
+                    const cellComponent = getCellComponent({
+                      row,
+                      column,
+                      datePattern,
+                    });
+                    return html` <td
+                      @click="${this.handleCellClick(row, column)}"
+                      style="${getStyle(
+                        this.columnsStyle,
+                        column.name,
+                        "cell"
+                      )}"
+                    >
                       ${cellComponent}
                     </td>`;
                   })}
@@ -245,9 +293,9 @@ export class MvTable extends LitElement {
                     : html``}
                 </tr>
               `;
-          })}
-        </tbody>
-      </table>
+            })}
+          </tbody>
+        </table>
       </div>
     `;
   }
@@ -261,18 +309,20 @@ export class MvTable extends LitElement {
   }
 
   handleClickCheckbox(event) {
-    const { detail: { value, checked, originalEvent } } = event;
+    const {
+      detail: { value, checked, originalEvent },
+    } = event;
     this.selectRow(value, checked, originalEvent);
   }
 
   handleRowClick(row) {
-    return originalEvent => {
+    return (originalEvent) => {
       this.dispatchEvent(
         new CustomEvent("row-click", { detail: { row, originalEvent } })
       );
-      if (this.selectable) {
+      if (this.selectable || this.selectOne) {
         const isSelected = this.selectedRows.some(
-          selectedRow => selectedRow === row
+          (selectedRow) => selectedRow === row
         );
         this.selectRow(row, !isSelected, originalEvent);
       }
@@ -280,7 +330,7 @@ export class MvTable extends LitElement {
   }
 
   handleCellClick(row, column) {
-    return originalEvent => {
+    return (originalEvent) => {
       if (!row && !column) {
         originalEvent.stopPropagation();
       } else {
@@ -288,7 +338,7 @@ export class MvTable extends LitElement {
         const value = row[name];
         this.dispatchEvent(
           new CustomEvent("cell-click", {
-            detail: { row, column, value, originalEvent }
+            detail: { row, column, value, originalEvent },
           })
         );
       }
@@ -299,36 +349,43 @@ export class MvTable extends LitElement {
     const isAllSelected = this.hasAllSelected();
     let removed = [];
     let added = [];
-    if (row === SELECT_ALL) {
-      if (isAllSelected) {
-        removed = [...this.selectedRows];
-        added = [];
-        this.selectedRows = [];
-        this.isAllSelected = false;
-      } else {
-        removed = [];
-        added = this.rows.filter(
-          item => !this.selectedRows.some(selectedRow => selectedRow === item)
-        );
-        this.selectedRows = [...this.rows];
-        this.isAllSelected = true;
-      }
+    if (this.selectOne) {
+      removed = [...this.selectedRows];
+      added = [row];
+      this.selectedRows = [row];
     } else {
-      if (checked) {
-        removed = [];
-        added = [row];
-        this.selectedRows = [...this.selectedRows, row];
-        this.isAllSelected = this.hasAllSelected();
-      } else {
-        const index = this.selectedRows.indexOf(row);
-        if (index > -1) {
-          removed = [this.selectedRows[index]];
+      if (row === SELECT_ALL) {
+        if (isAllSelected) {
+          removed = [...this.selectedRows];
           added = [];
-          this.selectedRows = [
-            ...this.selectedRows.slice(0, index),
-            ...this.selectedRows.slice(index + 1)
-          ];
+          this.selectedRows = [];
           this.isAllSelected = false;
+        } else {
+          removed = [];
+          added = this.rows.filter(
+            (item) =>
+              !this.selectedRows.some((selectedRow) => selectedRow === item)
+          );
+          this.selectedRows = [...this.rows];
+          this.isAllSelected = true;
+        }
+      } else {
+        if (checked) {
+          removed = [];
+          added = [row];
+          this.selectedRows = [...this.selectedRows, row];
+          this.isAllSelected = this.hasAllSelected();
+        } else {
+          const index = this.selectedRows.indexOf(row);
+          if (index > -1) {
+            removed = [this.selectedRows[index]];
+            added = [];
+            this.selectedRows = [
+              ...this.selectedRows.slice(0, index),
+              ...this.selectedRows.slice(index + 1),
+            ];
+            this.isAllSelected = false;
+          }
         }
       }
     }
@@ -338,8 +395,8 @@ export class MvTable extends LitElement {
           originalEvent,
           removed,
           added,
-          selected: this.selectedRows
-        }
+          selected: this.selectedRows,
+        },
       })
     );
   }
@@ -348,7 +405,7 @@ export class MvTable extends LitElement {
     return (
       this.rows.length > 0 &&
       this.selectedRows.length > 0 &&
-      this.rows.every(row => this.selectedRows.includes(row))
+      this.rows.every((row) => this.selectedRows.includes(row))
     );
   }
 }
